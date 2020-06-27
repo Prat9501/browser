4dc1545bfc7c96c4a1e0abfddb09e351c8576772
Prateek Gupta <prateekg.9501@gmail.com>
2020-06-27 12:58:46 +0530

    handling tabs

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Tabs from './components/Tabs';
 import AddressBar from './components/AddressBar';
 import './App.css';
 
 function App() {
+  const [browsers, setBrowser] = useState([
+    "https://learn.chrisoncode.io",
+    "https://dev.to"
+  ])
+  const [activeBrowser, setActiveBrowser] = useState(0);
+  const url = browsers[activeBrowser];
+
+  function chooseBrowser(id) {
+    setActiveBrowser(id);
+  }
+
+  function addBrowser() {
+    const newBrowsers = [...browsers, ""]
+    setBrowser(newBrowsers);
+    setActiveBrowser(newBrowsers.length - 1);
+  }
+
+  function updateBrowser(){
+    const newBrowsers = [...browsers];
+    newBrowsers[activeBrowser] = url;
+    setBrowser(newBrowsers);
+  }
+
   return (
     <div className="app">
       <div className="browser">
-        <Tabs />
+        <Tabs browsers={browsers} active={activeBrowser}
+          choose={chooseBrowser}
+          add={addBrowser} />
 
-        <AddressBar />
+        <AddressBar 
+          update={updateBrowser}
+          url={url}
+          />
 
         <div className="viewport">
-          <>New Tab Page</>
+          {url ? (
+            <iframe src={url}
+            title="Something" />
+          ): (<>New Tab Page </>)}
         </div>
       </div>
     </div>
